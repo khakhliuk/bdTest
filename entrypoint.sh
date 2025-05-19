48cd6f8636e1723a79553b20ee6c6651861cc09d
khakhliuk <maminakyrtka322@gmail.com>
2025-05-20 00:17:53 +0300

    entypointMore

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 /opt/mssql/bin/sqlservr &
+tail -f /var/opt/mssql/log/errorlog &
 
 echo "⏳ Чекаємо запуску SQL Server..."
 sleep 5
@@ -15,4 +16,4 @@ done
 echo "✅ SQL Server готовий! Відновлюємо базу..."
 
 # Відновлення з .bak
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -Q "RESTORE DATABASE [YourDb] FROM DISK = N'/var/opt/mssql/backup/YourDb.bak' WITH MOVE 'YourDb' TO '/var/opt/mssql/data/YourDb.mdf', MOVE 'YourDb_log' TO '/var/opt/mssql/data/YourDb_log.ldf', REPLACE"
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -Q "RESTORE DATABASE [kasinaq] FROM DISK = N'/var/opt/mssql/backup/YourDb.bak' WITH MOVE 'YourDb' TO '/var/opt/mssql/data/YourDb.mdf', MOVE 'YourDb_log' TO '/var/opt/mssql/data/YourDb_log.ldf', REPLACE"
