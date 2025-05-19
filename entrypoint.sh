a7f9a93188474f873527f0623d28c0c2fc740269
khakhliuk <maminakyrtka322@gmail.com>
2025-05-20 01:00:52 +0300

    anotherOne

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,21 +3,13 @@
 echo "⏳ Чекаємо запуск SQL Server..."
 
 # Чекаємо запуск SQL Server
-for i in {1..30}; do
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -Q "SELECT 1" &> /dev/null
-    if [ $? -eq 0 ]; then
-        echo "✅ SQL Server запущено!"
-        break
-    else
-        echo "❌ SQL Server ще не готовий, чекаємо 5 секунд..."
-        sleep 5
-    fi
-done
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -Q "SELECT 1" &> /dev/null
 
 echo "🚀 Відновлюємо базу даних з .bak..."
 
 # Відновлення бази
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -Q "RESTORE DATABASE [YourDb] FROM DISK = N'/var/opt/mssql/backup/YourDb.bak' WITH REPLACE, MOVE 'YourDb' TO '/var/opt/mssql/data/YourDb.mdf', MOVE 'YourDb_log' TO '/var/opt/mssql/data/YourDb_log.ldf'"
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -Q "RESTORE FILELISTONLY FROM DISK = N'/var/opt/mssql/backup/YourDb.bak'"
+
 
 echo "✅ Відновлення завершено."
 
