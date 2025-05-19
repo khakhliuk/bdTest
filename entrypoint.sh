907db9dc6f065fb9155bf68a66f0eb4b2150393e
khakhliuk <maminakyrtka322@gmail.com>
2025-05-20 01:03:42 +0300

    anotherOne

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,8 @@
 echo "⏳ Чекаємо запуск SQL Server..."
 
 # Чекаємо запуск SQL Server
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -Q "SELECT 1" &> /dev/null
+echo "🪵 Дивимось логи..."
+cat /var/opt/mssql/log/errorlog
 
-echo "🚀 Відновлюємо базу даних з .bak..."
+sleep infinity
 
-# Відновлення бази
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -Q "RESTORE FILELISTONLY FROM DISK = N'/var/opt/mssql/backup/YourDb.bak'"
-
-
-echo "✅ Відновлення завершено."
-
-tail -f /dev/null
